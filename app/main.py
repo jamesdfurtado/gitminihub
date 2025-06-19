adab7abbe3133e710f52fda80dff410071bd0211
James Furtado <jamesdfurtado@gmail.com>
2025-06-19 17:22:48 -0400

    Added user/repository URL endpoints (templates for now)

--- app/main.py
+++ app/main.py
@@ -5,6 +5,17 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
+# Homepage
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# Repositories
+@app.get("/{username}/{repo_name}", response_class=HTMLResponse)
+async def view_repo(request: Request, username: str, repo_name: str):
+    return templates.TemplateResponse("repo.html", {
+        "request": request,
+        "username": username,
+        "repo_name": repo_name
+    })
+
