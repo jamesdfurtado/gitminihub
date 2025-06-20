e246523ecc3ece0732aba53f8a405028af88fd0b
James Furtado <jamesdfurtado@gmail.com>
2025-06-20 13:35:05 -0400

    Added `auth.py` route

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.pages import homepage, user, repo
+from app.pages import homepage, user, repo, auth  # ← add auth here
 
 app = FastAPI()
 
 app.include_router(homepage.router)
 app.include_router(user.router)
 app.include_router(repo.router)
+app.include_router(auth.router)
