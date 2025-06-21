5f25b5d0915a43e280af3b4d4bea46212828ab17
James Furtado <jamesdfurtado@gmail.com>
2025-06-21 13:46:56 -0400

    mounted static/ directory

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.pages import homepage, user, repo, signup, login
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(signup.router)
 app.include_router(login.router)
