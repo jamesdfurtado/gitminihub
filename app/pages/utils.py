9a03a9ea6a887f89e3f5417c12bccc3180120972
James Furtado <jamesdfurtado@gmail.com>
2025-06-22 12:59:22 -0400

    Created login/signup helper functions

--- app/pages/utils.py
+++ app/pages/utils.py
@@ -1,6 +1,11 @@
 import os, json
+from passlib.hash import bcrypt
 
+# user content "database"
 users_path = "app/data/users.json"
+
+# These usernames cannot be used
+RESERVED_USERNAMES = {"login", "signup"}
 
 def load_users():
     if os.path.exists(users_path):
@@ -11,3 +16,26 @@ def load_users():
 def save_users(users):
     with open(users_path, "w") as f:
         json.dump(users, f, indent=2)
+
+def normalize_username(username: str) -> str:
+    return username.replace(" ", "").lower()
+
+def is_invalid_username(username: str) -> bool:
+    return (
+        not username
+        or " " in username
+        or any(c.isupper() for c in username)
+        or normalize_username(username) in RESERVED_USERNAMES
+    )
+
+# Reject passwords with invalid characters
+def is_invalid_password(password: str) -> bool:
+    return " " in password
+
+# Hash passwords
+def hash_password(password: str) -> str:
+    return bcrypt.hash(password)
+
+# Verify incoming passwords
+def verify_password(input_password: str, stored_hash: str) -> bool:
+    return bcrypt.verify(input_password, stored_hash)
