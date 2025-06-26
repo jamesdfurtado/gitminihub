dc4283f72ac28a99430e90ec6e0f9fe55f291b3c
James Furtado <jamesdfurtado@gmail.com>
2025-06-26 15:51:15 -0400

    Include backend API router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from app.pages import homepage, user, repo, signup, login, logout
+from app.api import remote
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,10 +19,13 @@ async def disable_caching(request: Request, call_next):
 # Mount static files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-# Include routers
+# Frontend routers
 app.include_router(signup.router)
 app.include_router(login.router)
 app.include_router(logout.router)
 app.include_router(homepage.router)
 app.include_router(repo.router)
 app.include_router(user.router)
+
+# Backend routers
+app.include_router(remote.router)
