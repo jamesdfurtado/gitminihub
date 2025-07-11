56b5952f7f3d449f5777d9b582da2660c5db7efc
James Furtado <jamesdfurtado@gmail.com>
2025-07-11 11:55:49 -0400

    Added cli auth routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
-from app.pages import homepage, user, repo, signup, login, logout
-from app.api import remote_repo
+from app.pages import homepage, user, repo, signup, login, logout, cli_login
+from app.api import remote_repo, cli_auth
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,13 +16,16 @@ async def disable_caching(request: Request, call_next):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Frontend routers
 app.include_router(signup.router)
 app.include_router(login.router)
 app.include_router(logout.router)
 app.include_router(homepage.router)
+
+
+app.include_router(cli_auth.router)
+app.include_router(cli_login.router)
+
 app.include_router(repo.router)
 app.include_router(user.router)
 
-# Backend routers
 app.include_router(remote_repo.router)
